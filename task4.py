1e7be99dc2e689ee86d9daa98c3c962fb6aa0281
MISHA-414 <misha1.petruhin@gmail.com>
2025-12-05 18:38:52 +0700

    1-7

--- task4.py
+++ task4.py
@@ -8,3 +8,17 @@
 """
 
 # Решение будет здесь
+n = 8
+dp = [[0] * n for _ in range(n)]
+dp[0][0] = 1
+
+for i in range(n):
+    for j in range(n):
+        if i > 0:
+            dp[i][j] += dp[i - 1][j]
+        if j > 0:
+            dp[i][j] += dp[i][j - 1]
+        if i > 0 and j > 0:
+            dp[i][j] += dp[i - 1][j - 1]
+
+print(dp[n - 1][n - 1])
